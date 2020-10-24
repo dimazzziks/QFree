b0e94368410ea94587b427068672a707056d85a8
Maxim <s_maxim_v@bk.ru>
2020-10-24 22:32:23 +0300

    Install Firebase pod

--- QFree/QFree/SceneDelegate.swift
+++ QFree/QFree/SceneDelegate.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import Firebase
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        FirebaseApp.configure()
+        
         guard let mainScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: mainScene)
         window?.overrideUserInterfaceStyle = .light
