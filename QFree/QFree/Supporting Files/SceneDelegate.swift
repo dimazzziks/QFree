6cce682c6439779341be2642764517d8ad463416
Maxim <maximsidorov@yandex-team.ru>
2020-12-01 12:41:12 +0300

    nothing

--- QFree/QFree/Supporting Files/SceneDelegate.swift
+++ QFree/QFree/Supporting Files/SceneDelegate.swift
@@ -13,7 +13,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
         FirebaseApp.configure()
         
         #if DEBUG
