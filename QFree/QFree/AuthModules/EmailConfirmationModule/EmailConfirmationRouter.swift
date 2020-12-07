9569e5f76d6b0d9898c4771be809161734b50706
Artem Kozlov <askozlov_10@edu.hse.ru>
2020-12-07 18:56:53 +0300

    minor fixes

--- QFree/QFree/AuthModules/EmailConfirmationModule/EmailConfirmationRouter.swift
+++ QFree/QFree/AuthModules/EmailConfirmationModule/EmailConfirmationRouter.swift
@@ -10,3 +10,11 @@ import Foundation
 protocol EmailConfirmationRouterProtocol {
         var viewController: EmailConfirmationViewProtocol { get set }
 }
+
+class EmailConfirmationRouter : EmailConfirmationRouterProtocol{
+    var viewController: EmailConfirmationViewProtocol
+    
+    init(view : EmailConfirmationViewProtocol){
+        self.viewController = view
+    }
+}
